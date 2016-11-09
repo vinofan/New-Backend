ba2e189cdafb8241c11064df77ee73c29af08f2f
vinofan <614068173@qq.com>
2016-11-09 15:50:14 +0800

    common page test

---
+++ resources/views/common.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>PPIN Tools</title>
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link rel="stylesheet" href="../css/bootstrap.min.css">
+  <link rel="stylesheet" href="../css/font-awesome.min.css">
+  <link rel="stylesheet" href="../css/ionicons.min.css">
+  <link rel="stylesheet" href="../css/AdminLTE.min.css">
+  <link rel="stylesheet" href="../css/skins/skin-purple.min.css">
+</head>
+<body class="hold-transition skin-purple sidebar-mini">
+<div class="wrapper">
+
+<!-- Header -->
+@include('common/header')
+<!-- Left sidebar -->
+@include('common/main_sidebar')
+<!-- Content -->
+<div class="content-wrapper">
+    <section class="content-header">
+        <h1>
+            {{ $page_title or "Page Title" }}
+            <small>{{ $page_description or null }}</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+            <li class="active">Here</li>
+        </ol>
+    </section>
+
+    <section class="content">
+        <!-- Page Content -->
+        @yield('content')
+    </section>
+</div>
+
+<!-- Footer -->
+@include('common/footer')
+<!-- Control Sidebar -->
+@include('common/control_sidebar')
+</div>
+
+<script src="../js/jquery-2.2.3.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+<script src="../js/app.min.js"></script>
+</body>
+</html>
---
+++ resources/views/common/header.blade.php
@@ -0,0 +1,55 @@
+  <header class="main-header">
+
+    <!-- Logo -->
+    <a href="index2.html" class="logo">
+      <span class="logo-mini"><b>IN</b>T</span>
+      <span class="logo-lg">PPIN BACKEND</span>
+    </a>
+
+    <!-- Header Navbar -->
+    <nav class="navbar navbar-static-top" role="navigation">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+      <!-- Navbar Right Menu -->
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          <!-- User Account Menu -->
+          <li class="dropdown user user-menu">
+            <!-- Menu Toggle Button -->
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <!-- The user image in the navbar-->
+              <img src="../img/avatar.jpg" class="user-image" alt="User Image">
+              <!-- hidden-xs hides the username on small devices so only the image appears. -->
+              <span class="hidden-xs">{{ $username or 'couponsn'}}</span>
+            </a>
+            <ul class="dropdown-menu">
+              <!-- The user image in the menu -->
+              <li class="user-header">
+                <img src="../img/avatar.jpg" class="img-circle" alt="User Image">
+
+                <p>
+                  {{ $userdesc or 'here is description' }}
+                  <small>{{ $accesstime or 'here is accesstime' }}</small>
+                </p>
+              </li>
+              <!-- Menu Footer-->
+              <li class="user-footer">
+                <div class="pull-left">
+                  <a href="#" class="btn btn-default btn-flat">Profile</a>
+                </div>
+                <div class="pull-right">
+                  <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                </div>
+              </li>
+            </ul>
+          </li>
+          <!-- Control Sidebar Toggle Button -->
+          <li>
+            <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
